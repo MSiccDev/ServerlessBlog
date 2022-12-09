f07733e2e209be8b0bc1ca3513eea9ab75c389bb
Marco Siccardi <68176309+MSicc@users.noreply.github.com>
2022-12-09 19:41:52 +0100

    moved to isolated process

--- src/BlogFunctions/AuthorFunction.cs
+++ src/BlogFunctions/AuthorFunction.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using MSiccDev.ServerlessBlog.EFCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using MSiccDev.ServerlessBlog.ModelHelper;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.AspNetCore.Http.Extensions;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using MSiccDev.ServerlessBlog.EntityModel;
 
 namespace MSiccDev.ServerlessBlog.BlogFunctions
 {
@@ -21,80 +15,81 @@ namespace MSiccDev.ServerlessBlog.BlogFunctions
     {
         private const string Route = "blog/{blogId}/author";
 
-        public AuthorFunction(BlogContext blogContext) : base(blogContext)
+        public AuthorFunction(BlogContext blogContext, ILoggerFactory loggerFactory) : base(blogContext)
         {
+            _logger = loggerFactory.CreateLogger<AuthorFunction>();
         }
 
-        [FunctionName($"{nameof(AuthorFunction)}_{nameof(Create)}")]
-        public override async Task<IActionResult> Create([HttpTrigger(AuthorizationLevel.Function, new[] { "post" }, Route = Route)] HttpRequest req, ILogger log, string blogId)
+        [Function($"{nameof(AuthorFunction)}_{nameof(Create)}")]
+        public override async Task<HttpResponseData> Create([HttpTrigger(AuthorizationLevel.Function, new[] { "post" }, Route = Route)] HttpRequestData req, string blogId)
         {
             try
             {
                 string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
 
-                DtoModel.Author author = JsonConvert.DeserializeObject<DtoModel.Author>(requestBody);
+                DtoModel.Author? author = JsonConvert.DeserializeObject<DtoModel.Author>(requestBody);
 
                 if (author != null)
                 {
 
-                    EntityModel.Author newAuthorEntity = author.CreateFrom(Guid.Parse(blogId));
+                    Author? newAuthorEntity = author.CreateFrom(Guid.Parse(blogId));
 
-                    EntityEntry<EntityModel.Author> createdAuthor =
+                    EntityEntry<Author> createdAuthor =
                         _blogContext.Authors.Add(newAuthorEntity);
 
                     await _blogContext.SaveChangesAsync();
 
-                    return new CreatedResult($"{req.GetEncodedUrl()}/{createdAuthor.Entity.AuthorId}", "OK");
+                    return await req.CreateNewEntityCreatedResponseDataAsync(createdAuthor.Entity.AuthorId);
                 }
                 else
                 {
-                    return new BadRequestObjectResult("Submitted data is invalid, author cannot be created.");
+                    return await req.CreateResponseDataAsync(System.Net.HttpStatusCode.BadRequest, "Submitted data is invalid, author cannot be created.");
                 }
             }
             catch (Exception ex)
             {
                 //TODO: better handling of these cases...
-                return new BadRequestObjectResult(ex);
+                return await req.CreateResponseDataAsync(System.Net.HttpStatusCode.BadRequest, ex.ToString());
             }
         }
 
-        [FunctionName($"{nameof(AuthorFunction)}_{nameof(Delete)}")]
-        public override async Task<IActionResult> Delete([HttpTrigger(AuthorizationLevel.Function, new[] { "delete" }, Route = Route + "/{id}")] HttpRequest req, ILogger log, string blogId, string id)
+        [Function($"{nameof(AuthorFunction)}_{nameof(Delete)}")]
+        public override async Task<HttpResponseData> Delete([HttpTrigger(AuthorizationLevel.Function, new[] { "delete" }, Route = Route + "/{id}")] HttpRequestData req, string blogId, string id)
         {
             try
             {
-                EntityModel.Author existingAuthor = await _blogContext.Authors.
+                EntityModel.Author? existingAuthor = await _blogContext.Authors.
                                                         Include(author => author.UserImage).
                                                         SingleOrDefaultAsync(author => author.BlogId == Guid.Parse(blogId) &&
                                                                                      author.AuthorId == Guid.Parse(id));
 
                 if (existingAuthor == null)
                 {
-                    log.LogWarning($"Author with Id {id} not found");
-                    return new NotFoundResult();
+                    _logger.LogWarning($"Author with Id {id} not found");
+                    return req.CreateResponse(HttpStatusCode.NotFound);
                 }
 
                 _blogContext.Authors.Remove(existingAuthor);
 
                 await _blogContext.SaveChangesAsync();
 
-                return new OkResult();
+                return req.CreateResponse(HttpStatusCode.OK);
             }
             catch (Exception ex)
             {
                 //TODO: better handling of these cases...
-                return new BadRequestObjectResult(ex);
+                return await req.CreateResponseDataAsync(HttpStatusCode.BadRequest, ex.ToString());
             }
         }
 
-        [FunctionName($"{nameof(AuthorFunction)}_{nameof(Get)}")]
-        public override async Task<IActionResult> Get([HttpTrigger(AuthorizationLevel.Function, new[] { "get" }, Route = Route + "/{id?}")] HttpRequest req, ILogger log, string blogId, string id = null)
+        [Function($"{nameof(AuthorFunction)}_{nameof(Get)}")]
+        public override async Task<HttpResponseData> Get([HttpTrigger(AuthorizationLevel.Function, new[] { "get" }, Route = Route + "/{id?}")] HttpRequestData req, string blogId, string? id = null)
         {
             try
             {
                 if (!string.IsNullOrWhiteSpace(id))
                 {
-                    EntityModel.Author existingAuthor =
+                    EntityModel.Author? existingAuthor =
                             await _blogContext.Authors.
                                    Include(author => author.UserImage).
                                    ThenInclude(media => media.MediumType).
@@ -103,55 +98,53 @@ namespace MSiccDev.ServerlessBlog.BlogFunctions
 
                     if (existingAuthor == null)
                     {
-                        log.LogWarning($"Author with Id {id} not found");
-                        return new NotFoundResult();
+                        _logger.LogWarning($"Author with Id {id} not found");
+                        return req.CreateResponse(HttpStatusCode.NotFound);
                     }
 
-                    return new OkObjectResult(JsonConvert.SerializeObject(existingAuthor.ToDto(), _jsonSerializerSettings));
+                    return await req.CreateOKResponseDataWithJsonAsync(existingAuthor.ToDto());
                 }
                 else
                 {
-                    log.LogInformation("Trying to get authors...");
+                    _logger.LogInformation("Trying to get authors...");
 
                     var queryParams = req.GetQueryParameterDictionary();
 
-                    if (blogId == default)
-                        return new BadRequestObjectResult("Required parameter 'blogid' (GUID) is not specified or cannot be parsed.");
-
-                    List<EntityModel.Author> entityResultSet = new List<EntityModel.Author>();
+                    if ((string.IsNullOrWhiteSpace(blogId) || Guid.Parse(blogId) == default))
+                        return await req.CreateResponseDataAsync(HttpStatusCode.BadRequest,"Required parameter 'blogId' (GUID) is not specified or cannot be parsed.");
 
                     (int count, int skip) = req.GetPagingProperties();
 
-                    entityResultSet = await _blogContext.Authors.
-                                            Where(author => author.BlogId == Guid.Parse(blogId)).
-                                            Skip(skip).
-                                            Take(count).
-                                            ToListAsync();
+                    List<EntityModel.Author> entityResultSet = await _blogContext.Authors.
+                                                                                  Where(author => author.BlogId == Guid.Parse(blogId)).
+                                                                                  Skip(skip).
+                                                                                  Take(count).
+                                                                                  ToListAsync();
 
                     List<DtoModel.Author> resultSet = entityResultSet.Select(entity => entity.ToDto()).ToList();
 
-                    return new OkObjectResult(JsonConvert.SerializeObject(resultSet, _jsonSerializerSettings));
+                    return await req.CreateOKResponseDataWithJsonAsync(resultSet);
                 }
             }
             catch (Exception ex)
             {
                 //TODO: better handling of these cases...
-                return new BadRequestObjectResult(ex);
+                return await req.CreateResponseDataAsync(HttpStatusCode.BadRequest, ex.ToString());
             }
         }
 
-        [FunctionName($"{nameof(AuthorFunction)}_{nameof(Update)}")]
-        public override async Task<IActionResult> Update([HttpTrigger(AuthorizationLevel.Function, new[] { "put" }, Route = Route + "/{id}")] HttpRequest req, ILogger log, string blogId, string id)
+        [Function($"{nameof(AuthorFunction)}_{nameof(Update)}")]
+        public override async Task<HttpResponseData> Update([HttpTrigger(AuthorizationLevel.Function, new[] { "put" }, Route = Route + "/{id}")] HttpRequestData req,  string blogId, string id)
         {
             try
             {
                 string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
 
-                DtoModel.Author author = JsonConvert.DeserializeObject<DtoModel.Author>(requestBody);
+                DtoModel.Author? author = JsonConvert.DeserializeObject<DtoModel.Author>(requestBody);
 
                 if (author != null)
                 {
-                    EntityModel.Author existingAuthor =
+                    EntityModel.Author? existingAuthor =
                             await _blogContext.Authors.
                                    Include(author => author.UserImage).
                                    ThenInclude(media => media.MediumType).
@@ -160,25 +153,25 @@ namespace MSiccDev.ServerlessBlog.BlogFunctions
 
                     if (existingAuthor == null)
                     {
-                        log.LogWarning($"Author with Id {id} not found");
-                        return new NotFoundResult();
+                        _logger.LogWarning($"Author with Id {id} not found");
+                        return req.CreateResponse(HttpStatusCode.NotFound);
                     }
 
                     existingAuthor.UpdateWith(author);
 
                     await _blogContext.SaveChangesAsync();
 
-                    return new AcceptedResult();
+                    return req.CreateResponse(HttpStatusCode.Accepted);
                 }
                 else
                 {
-                    return new BadRequestObjectResult("Submitted data is invalid, author cannot be modified.");
+                    return await req.CreateResponseDataAsync(HttpStatusCode.BadRequest, "Submitted data is invalid, author cannot be modified.");
                 }
             }
             catch (Exception ex)
             {
                 //TODO: better handling of these cases...
-                return new BadRequestObjectResult(ex);
+                return await req.CreateResponseDataAsync(HttpStatusCode.BadRequest, ex.ToString());
             }
         }
 
